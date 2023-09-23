d686cb264a68819fc55740fd7556f8067854f0c7
Soist <1332193389@qq.com>
2023-09-23 16:25:13 -0400

    add GrammarOptionsPage

---
+++ src/components/StudyOptionsPage/GrammarOptionsPage.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { SUGAR_HONEYCOMBS, STUDY_OPTIONS_PATH } from '../../constants';
+import './StudyOptionsPage.css';
+
+function GrammarOptionsPage() {
+    const location = useLocation();
+    const unit = location.state;
+
+    return (
+        <div id='study-options-page-container'>
+            <h1>{unit.name} Grammar</h1>
+            <Link to={SUGAR_HONEYCOMBS} state={unit}>
+                <button className='btn btn-primary'>
+                    {'Sugar Honeycombs'}
+                </button>
+            </Link>
+            <Link to={STUDY_OPTIONS_PATH} state={unit}>
+                <button className='btn btn-primary'>
+                    {'Back'}
+                </button>
+            </Link>
+        </div>
+    );
+}
+
+export default GrammarOptionsPage;
