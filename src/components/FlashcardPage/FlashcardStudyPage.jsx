0c8ee5954d22872bb07c76aadb230d8825bec7a6
Sarah Mabrouk <smabrouk6@Sarahs-MacBook-Air.local>
2023-09-28 16:33:13 -0400

    User can now scroll vertically in image preview to accomodate large images

--- src/components/FlashcardPage/FlashcardStudyPage.jsx
+++ src/components/FlashcardPage/FlashcardStudyPage.jsx
@@ -1,22 +1,114 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { getVocab } from '../../vocabData';
 import Flashcard from './Flashcard';
 import './FlashcardStudyPage.css';
 
-
 function FlashcardStudyPage() {
-    const location = useLocation();
-    const unit = location.state;
-    const cards = getVocab(unit.number);
+  const location = useLocation();
+  const unit = location.state;
+  const initialCards = getVocab(unit.number);
 
-    return (
-        <div id='flashcard-container'>
-            {cards.map(flashcard => {
-                return <Flashcard flashcard={flashcard} />
-            })}
+  const [flashcards, setFlashcards] = useState(initialCards);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [koreanText, setKoreanText] = useState('');
+  const [imageURL, setImageURL] = useState('');
+  const [selectedFlashcardIndex, setSelectedFlashcardIndex] = useState(null);
+
+  const handleAddFlashcard = (index) => {
+    setSelectedFlashcardIndex(index);
+    setIsModalVisible(true);
+  };
+
+  const handleKoreanTextChange = (e) => {
+    setKoreanText(e.target.value);
+  };
+
+  const handleImageURLChange = (e) => {
+    setImageURL(e.target.value);
+  };
+
+  const handleSaveFlashcard = () => {
+    if (selectedFlashcardIndex !== null) {
+      const updatedFlashcards = [...flashcards];
+      updatedFlashcards[selectedFlashcardIndex] = {
+        ...updatedFlashcards[selectedFlashcardIndex],
+        korean: koreanText,
+        image: imageURL,
+      };
+      setFlashcards(updatedFlashcards);
+    } else {
+      const newFlashcard = {
+        korean: koreanText,
+        image: imageURL,
+      };
+      setFlashcards([...flashcards, newFlashcard]);
+    }
+
+    setIsModalVisible(false);
+    setKoreanText('');
+    setSelectedFlashcardIndex(null);
+    setImageURL('');
+  };
+
+  const handleCloseModal = () => {
+    setIsModalVisible(false);
+    setKoreanText('');
+    setImageURL('');
+    setSelectedFlashcardIndex(null);
+  };
+
+  return (
+    <div id='flashcard-container'>
+      {flashcards.map((flashcard, index) => (
+        <Flashcard
+          key={index}
+          flashcard={flashcard}
+          imageURL={flashcard.image}
+          onAddFlashcard={() => handleAddFlashcard(index)}
+        />
+      ))}
+      <div className='add-flashcard-button'>
+        <button onClick={() => handleAddFlashcard(null)}>Add Flashcard</button>
+      </div>
+      {isModalVisible && (
+        <div className='modal-popup'>
+          <div className='flashcard-modal'>
+            <h2>Add/Edit a Flashcard</h2>
+            <div className='modal-content'>
+              <label>
+                Korean Text:
+                <input
+                  type='text'
+                  value={koreanText}
+                  onChange={handleKoreanTextChange}
+                  required
+                />
+              </label>
+              <label>
+                Image URL:
+                <input
+                  type='text'
+                  value={imageURL}
+                  onChange={handleImageURLChange}
+                  required
+                />
+              </label>
+              {imageURL && (
+                <div className='image-preview'>
+                  <img src={imageURL} alt='Image Preview' />
+                </div>
+              )}
+            </div>
+            <div className='modal-buttons'>
+              <button onClick={handleSaveFlashcard}>Save</button>
+              <button onClick={handleCloseModal}>Cancel</button>
+            </div>
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default FlashcardStudyPage;
