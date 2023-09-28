9af69f477bc025fccfdea8ebc3cedcc502127252
Sarah Mabrouk <smabrouk6@lawn-10-91-73-188.lawn.gatech.edu>
2023-09-28 10:15:57 -0400

    User can now view image they added to the flashcard including an image preview

--- src/components/FlashcardPage/Flashcard.jsx
+++ src/components/FlashcardPage/Flashcard.jsx
@@ -2,34 +2,43 @@ import React, { useState } from 'react';
 import { getAudio } from '../../audioData';
 import { ReactComponent as SoundSvg } from './sound.svg';
 
-const Flashcard = ({ flashcard }) => {
-    const [flip, setFlip] = useState(false);
-    const sound = getAudio(flashcard.korean) === null ? null : new Audio(require(`../../audios/words/${getAudio(flashcard.korean)}`));
-    const playSound = (event) => {
-        event.stopPropagation();
-        sound.play();
-    };
+const Flashcard = ({ flashcard, imageURL }) => {
+  const [flip, setFlip] = useState(false);
+  const sound = getAudio(flashcard.korean) === null ? null : new Audio(require(`../../audios/words/${getAudio(flashcard.korean)}`));
+  const playSound = (event) => {
+    event.stopPropagation();
+    sound.play();
+  };
 
-    return (
-        <div className={`card${flip ? ' flip' : ''}`} onClick={() => setFlip(!flip)}>
-            <div className='front'>
-                <div className='content-container'><div className='content'>{flashcard.english}</div></div>
-                {sound !== null &&
-                    <div className='sound' onClick={playSound}>
-                        <SoundSvg />
-                    </div>
-                }
+  return (
+    <div className={`card${flip ? ' flip' : ''}`} onClick={() => setFlip(!flip)}>
+      <div className='front'>
+        <div className='content-container'>
+            {imageURL && (
+            <div className='image-preview'>
+              <img src={imageURL} alt='Image Preview' />
             </div>
-            <div className='back'>
-                <div className='content-container'><div className='content'>{flashcard.korean}</div></div>
-                {sound !== null &&
-                    <div className='sound' onClick={playSound}>
-                        <SoundSvg />
-                    </div>
-                }
+          )}
+          <div className='content-wrapper'>
+            <div className='content'>{flashcard.english}</div>
+            {sound !== null && 
+              <div className='sound' onClick={playSound}>
+                <SoundSvg />
+              </div>
+            }
+          </div>
+        </div>
+      </div>
+      <div className='back'>
+      <div className='content-container'><div className='content'>{flashcard.korean}</div></div>
+          {sound !== null && 
+            <div className='sound' onClick={playSound}>
+              <SoundSvg />
             </div>
-        </div >
-    );
+          }
+        </div>
+      </div >
+  );
 }
 
 export default Flashcard;
