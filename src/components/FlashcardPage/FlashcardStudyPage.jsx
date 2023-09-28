63b94ed1ae4ac1e28a293f84ef69d7244be813a1
Sarah Mabrouk <smabrouk6@Sarahs-MacBook-Air.local>
2023-09-28 01:24:28 -0400

    Displaying Image User Adds to flashcards including image preview

--- src/components/FlashcardPage/FlashcardStudyPage.jsx
+++ src/components/FlashcardPage/FlashcardStudyPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { getVocab } from '../../vocabData';
 import Flashcard from './Flashcard';
+
 import './FlashcardStudyPage.css';
 
 function FlashcardStudyPage() {
@@ -13,8 +14,10 @@ function FlashcardStudyPage() {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [koreanText, setKoreanText] = useState('');
   const [imageURL, setImageURL] = useState('');
+  const [selectedFlashcardIndex, setSelectedFlashcardIndex] = useState(null);
 
-  const handleAddFlashcard = () => {
+  const handleAddFlashcard = (index) => {
+    setSelectedFlashcardIndex(index);
     setIsModalVisible(true);
   };
 
@@ -27,13 +30,25 @@ function FlashcardStudyPage() {
   };
 
   const handleSaveFlashcard = () => {
-    const newFlashcard = {
-      korean: koreanText,
-      image: imageURL,
-    };
-    setFlashcards([...flashcards, newFlashcard]);
+    if (selectedFlashcardIndex !== null) {
+      const updatedFlashcards = [...flashcards];
+      updatedFlashcards[selectedFlashcardIndex] = {
+        ...updatedFlashcards[selectedFlashcardIndex],
+        korean: koreanText,
+        image: imageURL,
+      };
+      setFlashcards(updatedFlashcards);
+    } else {
+      const newFlashcard = {
+        korean: koreanText,
+        image: imageURL,
+      };
+      setFlashcards([...flashcards, newFlashcard]);
+    }
+
     setIsModalVisible(false);
     setKoreanText('');
+    setSelectedFlashcardIndex(null);
     setImageURL('');
   };
 
@@ -41,39 +56,52 @@ function FlashcardStudyPage() {
     setIsModalVisible(false);
     setKoreanText('');
     setImageURL('');
+    setSelectedFlashcardIndex(null);
   };
 
   return (
-    <div id="flashcard-container">
+    <div id='flashcard-container'>
       {flashcards.map((flashcard, index) => (
-        <Flashcard key={index} flashcard={flashcard} />
+        <Flashcard
+          key={index}
+          flashcard={flashcard}
+          imageURL={flashcard.image}
+          onAddFlashcard={() => handleAddFlashcard(index)}
+        />
       ))}
-      <div className="add-flashcard-button">
-        <button onClick={handleAddFlashcard}>Add Flashcard</button>
+      <div className='add-flashcard-button'>
+        <button onClick={() => handleAddFlashcard(null)}>Add Flashcard</button>
       </div>
       {isModalVisible && (
-        <div className="modal-popup">
-          <div className="flashcard-modal">
-            <h2>Add a Flashcard</h2>
-            <label>
-              Korean Text:
-              <input
-                type="text"
-                value={koreanText}
-                onChange={handleKoreanTextChange}
-                required
-              />
-            </label>
-            <label>
-              Image URL:
-              <input
-                type="text"
-                value={imageURL}
-                onChange={handleImageURLChange}
-                required
-              />
-            </label>
-            <div className="modal-buttons">
+        <div className='modal-popup'>
+          <div className='flashcard-modal'>
+            <h2>Add/Edit a Flashcard</h2>
+            <div className='modal-content'>
+              <label>
+                Korean Text:
+                <input
+                  type='text'
+                  value={koreanText}
+                  onChange={handleKoreanTextChange}
+                  required
+                />
+              </label>
+              <label>
+                Image URL:
+                <input
+                  type='text'
+                  value={imageURL}
+                  onChange={handleImageURLChange}
+                  required
+                />
+              </label>
+              {imageURL && (
+                <div className='image-preview'>
+                  <img src={imageURL} alt='Image Preview' />
+                </div>
+              )}
+            </div>
+            <div className='modal-buttons'>
               <button onClick={handleSaveFlashcard}>Save</button>
               <button onClick={handleCloseModal}>Cancel</button>
             </div>
