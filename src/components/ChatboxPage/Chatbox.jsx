b948936d753e3d611d97c328de63554d0e362994
kavyaahuja <40362874+kavyaahuja@users.noreply.github.com>
2023-10-16 14:45:48 -0400

    Update Chatbox.jsx
    button functionality added to chatbox

--- src/components/ChatboxPage/Chatbox.jsx
+++ src/components/ChatboxPage/Chatbox.jsx
@@ -1,27 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Chatbox.css';
 
 function Chatbox() {
-    
+  const [messages, setMessages] = useState([
+
+    { user: true, text: 'Hello!' },
+    { user: false, text: 'Hi there! Ready to practice!' },
+
+  ]);
+
+  const [inputMessage, setInputMessage] = useState('');
+
+  const handleSendMessage = () => {
+    if (inputMessage.trim() !== '') {
+      setMessages([
+        ...messages,
+        { user: true, text: inputMessage },
+      ]);
+      setInputMessage('');
+
+
+      setTimeout(() => {
+        setMessages([
+          ...messages,
+          { user: false, text: 'This is a simulated bot response.' },
+        ]);
+      }, 1000);
+    }
+  };
+
   return (
     <div className="chatbox-container">
       <div className="chatbox-header">
         <h2>Chatbox</h2>
       </div>
       <div className="chatbox-messages">
-        <div className="chatbox-message">
-          <p>User: Hello!</p>
-        </div>
-        <div className="chatbox-message">
-          <p>Helper: Hi there! Ready to Practice!</p>
-        </div>
-        {/* Add more messages here */}
+        {messages.map((message, index) => (
+          <div
+            key={index}
+            className={`chatbox-message ${message.user ? 'user' : 'bot'}`}
+          >
+            <p>{message.text}</p>
+          </div>
+        ))}
       </div>
-      <input
-        type="text"
-        placeholder="Type your message..."
-        className="chatbox-input"
-      />
+      <div className="chatbox-input-container">
+        <input
+          type="text"
+          placeholder="Type your message..."
+          className="chatbox-input"
+          value={inputMessage}
+          onChange={(e) => setInputMessage(e.target.value)}
+        />
+        <button className="send-button" onClick={handleSendMessage}>
+          Send
+        </button>
+      </div>
     </div>
   );
 }
